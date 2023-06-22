5d59fcf27e1fe0bef11a7262f1285d59b50923bc
EGAMER26 <evandrogomes542@gmail.com>
2023-06-22 16:18:25 -0300

    feito a estilização no focus da pagina de cadastro de item

--- root/cadastro_produto_pagina.php
+++ root/cadastro_produto_pagina.php
@@ -9,6 +9,13 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Geologica:wght@100;200;400;700;900&display=swap" rel="stylesheet">
     <title>Cadastro de produto</title>
+    <style>
+        .cadastroProdWrapperInputBox {
+            display: flex;
+            flex-direction: column;
+            gap: 6rem;
+        }
+    </style>
 </head>
 <body>
     <div class="wrapper_login">
@@ -18,31 +25,33 @@
         <div class="login form_cadastro_produto">
             <img width="64" src="/assets/coppio_logo.png" alt="logo coppio">
             <form action="cadastro_produto.php" method="post">
-                    <div class="div_nome all_divs_form_cadastro_prod">
-                        <p>Nome</p>
-                        <input type="text" name="nome_prod_txt" id="email" placeholder="">
+                    <div class="cadastroProdWrapperInputBox">
+                        <div class="inputBox">
+                            <input type="text" name="nome_prod_txt" required id="inputUser">
+                            <label for="nome_prod_txt" class="labelInput">Nome do Item</label>
+                        </div>
+                        <div class="inputBox">
+                            <input type="text" name="desc_prod" required id="inputUser">
+                            <label for="desc_prod" class="labelInput">Descrição</label>
+                        </div>
+                                        <div class="qtd_nota">
+                        
+                                        <div class="inputBox">
+                                            <input type="number" name="qtd_prod" required id="inputUser">
+                                            <label for="qtd_prod" class="labelInput">Quantidade</label>                   
+                                        </div>
+                                        <div class="inputBox">
+                                            <input type="number" name="nota_fiscal" required id="inputUser">
+                                            <label for="nota_fiscal" class="labelInput">Nota Fiscal</label>
+                        
+                                        </div>
+                        
+                                        </div>
+                                        <div class="inputBox">
+                                            <input type="text" name="fornecedor_prod" required id="inputUser">
+                                            <label for="fornecedor_prod" class="labelInput">Fornecedor</label>                    
+                                        </div>
                     </div>
-                    <div class="div_desc all_divs_form_cadastro_prod">
-                        <p>Descrição</p>
-                        <input type="search" name="desc_prod" required id="email" placeholder="">
-                    </div>
-                <div class="qtd_nota">
-
-                <div class="div_qtd all_divs_form_cadastro_prod">
-                    <p>Quantidade</p>
-                    <input type="number" name="qtd_prod" required id="senha" placeholder="">
-                </div>
-                <div class="div_notaFiscal all_divs_form_cadastro_prod">
-                    <p>Nota Fiscal</p>
-                    <input type="number" name="nota_fiscal" required id="senha" placeholder="">
-
-                </div>
-
-                </div>
-                <div class="div_fornecedor all_divs_form_cadastro_prod">
-                    <p>Fornecedor</p>
-                    <input type="text" name="fornecedor_prod" required id="email" placeholder="">
-                </div>
 
                 <input type="submit" value="cadastrar" id="cadastro_prod">
             </form>
