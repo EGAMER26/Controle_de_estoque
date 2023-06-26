9c0bd764b72ed5a0fc5bc66ad6bee0a1d4c9aa16
EGAMER26 <evandrogomes542@gmail.com>
2023-06-26 10:56:39 -0300

    feito o back da pagina de cadastro de produto

--- root/back_cadastro_produto.php
+++ root/back_cadastro_produto.php
@@ -1,7 +1,11 @@
     <?php 
 
 $conn = new mysqli('127.0.0.1', 'root', 'usbw', 'controle_estoque');
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
 
+// include_once ('cadastro_produto_pagina');
 $nome_prod = $_POST['nome_prod_txt'];
 $descri_prod = $_POST['descricao_prod'];
 $qtd_prod = $_POST['qtd_prod'];
@@ -9,17 +13,19 @@ $nota_fiscal = $_POST["nota_fiscal"];
 $fornecedor = $_POST['fornecedor_prod'];
 $data = $_POST['data_prod'];
 
-$sql = "INSERT INTO material (nome_produto, desc_produto, valor_produto, nota_fiscal, forncedor, data_chegada) VALUES ('$nome_prod', '$descri_prod', '$qtd_prod', '$nota_fiscal', '$fornecedor', '$data')";
+$sql = "INSERT INTO material (nome_produto, desc_produto, valor_produto, nota_fiscal, fornecedor, data_chegada) VALUES ('$nome_prod', '$descri_prod', '$qtd_prod', '$nota_fiscal', '$fornecedor', '$data')";
 
 if ($conn->query($sql) === TRUE) {
     echo "<script>";
     echo "alert('Produto cadastrado!');";
-    echo "window.open('http://localhost:8080/produtos.php/', '_self');";
+    echo "window.open('http://localhost:8080/cadastro_produto_pagina.php', '_self');";
     echo "</script>";
 } else {
-    echo "<script>";
-    echo "alert('Erro ao cadastrar o produto!');";
-    echo "</script>";
+    // echo "<script>";
+    // echo "alert('Erro ao cadastrar o produto!');";
+    // echo "window.open('http://localhost:8080/cadastro_produto_pagina.php', '_self');";
+    // echo "</script>";
+    echo "Erro na consulta: " . $conn->error;
 }
 
 $conn->close();
